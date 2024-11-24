3a67c3936bdf0bc50171df6ccbc2c6668987c92f
Abeille Ewen <ewenabeille@gmail.com>
2024-11-24 15:42:04 +0100

    Add Windwos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+import java.awt.event.*;
+import view.SimpleFenetre;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                //On crée une nouvelle instance de notre JDialog
+                SimpleFenetre fenetre = new SimpleFenetre();
+                fenetre.setVisible(true);//On la rend visible
+            }
+        });
     }
 }
+
